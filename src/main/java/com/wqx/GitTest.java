e9d6826fc160b04c0d2b48f2a28dde4c9af70e66
wqx <wqx.@163.com>
2021-05-04 14:56:49 +0800

    push commit

--- src/main/java/com/wqx/GitTest.java
+++ src/main/java/com/wqx/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git1");
         System.out.println("hot-fix test");
+        System.out.println("master test");
+        System.out.println("push test");
 
     }
 }
