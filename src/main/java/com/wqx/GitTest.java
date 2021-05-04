51e43f0ef999bc4125fe920d8bbd8ef4b19a5454
wqx <wqx.@163.com>
2021-05-04 14:07:41 +0800

    second commit

--- src/main/java/com/wqx/GitTest.java
+++ src/main/java/com/wqx/GitTest.java
@@ -3,5 +3,6 @@ package com.wqx;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("idea 集成git的使用");
+        System.out.println("hello git");
     }
 }
