244d4ebd7ff65f9a455881f7528e2a03d0595adb
wqx <wqx.@163.com>
2021-05-04 14:19:20 +0800

    hot-fix commit2

--- src/main/java/com/wqx/GitTest.java
+++ src/main/java/com/wqx/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
         System.out.println("idea 集成git的使用");
         System.out.println("hello git");
         System.out.println("hello git1");
-        System.out.println("hello git2");
+        System.out.println("hot-fix test");
 
     }
 }
