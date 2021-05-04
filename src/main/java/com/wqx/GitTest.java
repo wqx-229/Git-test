974e6cc83624b5adeca5828a9ffcd0045dc22a9d
wqx <wqx.@163.com>
2021-05-04 14:08:18 +0800

    third commit

--- src/main/java/com/wqx/GitTest.java
+++ src/main/java/com/wqx/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("idea 集成git的使用");
         System.out.println("hello git");
+        System.out.println("hello git1");
+
     }
 }
